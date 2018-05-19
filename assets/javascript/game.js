fbe461008e3631397cf23da960d0284ea4726ab8
Xeroneon <andrewvasquez0113@gmail.com>
2018-05-19 15:27:37 -0700

    added radiant bg and changed text fields to be responsive for better experience on mobile

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,28 @@
 $(document).ready(function() {
 
     var random;
-    console.log(random);
+    // console.log(random);
+
+    //function to start board and reset board
     var startReset = function() {
         random = Math.floor(Math.random() * 101) + 19;
-        $("#rand-num").text(random);
+        $(".rand-num").text(random);
         for (i = 1; i < 5; i++) {
-            console.log("for loop ran");
+            // console.log("for loop ran");
             var crysNum = Math.floor(Math.random() * 12) + 1;
-            console.log(crysNum);
+            // console.log(crysNum);
             $("#cry-" + i).attr("crysRandNum", crysNum);
-            console.log($("#cry-" + i).attr("crysRandNum"));
+            // console.log($("#cry-" + i).attr("crysRandNum"));
         }
         points = 0;
-        $("#points").text(points);
+        $(".points").text(points);
     }
     startReset();
 
 
     var points = 0;
 
+    //function to alert win or loss and call the reset function
     var winLose = function() {
         if (points === random) {
             alert("Win");
@@ -32,34 +35,34 @@ $(document).ready(function() {
     }
 
     $("#cry-1").on("click", function() {
-        console.log($("#cry-1").attr("crysRandNum"));
+        // console.log($("#cry-1").attr("crysRandNum"));
         points = points + parseInt($("#cry-1").attr("crysRandNum"));
-        console.log(points);
-        $("#points").text(points);
+        // console.log(points);
+        $(".points").text(points);
         winLose();
     })
 
     $("#cry-2").on("click", function() {
-        console.log($("#cry-2").attr("crysRandNum"));
+        // console.log($("#cry-2").attr("crysRandNum"));
         points = points + parseInt($("#cry-2").attr("crysRandNum"));
-        console.log(points);
-        $("#points").text(points);
+        // console.log(points);
+        $(".points").text(points);
         winLose();
     })
 
     $("#cry-3").on("click", function() {
-        console.log($("#cry-3").attr("crysRandNum"));
+        // console.log($("#cry-3").attr("crysRandNum"));
         points = points + parseInt($("#cry-3").attr("crysRandNum"));
-        console.log(points);
-        $("#points").text(points);
+        // console.log(points);
+        $(".points").text(points);
         winLose();
     })
 
     $("#cry-4").on("click", function() {
-        console.log($("#cry-4").attr("crysRandNum"));
+        // console.log($("#cry-4").attr("crysRandNum"));
         points = points + parseInt($("#cry-4").attr("crysRandNum"));
-        console.log(points);
-        $("#points").text(points);
+        // console.log(points);
+        $(".points").text(points);
         winLose();
     })
 
