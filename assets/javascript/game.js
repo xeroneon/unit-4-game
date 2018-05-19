0d2c56c1a83e5e00dece1dd6bc2c2a5fee410952
Xeroneon <andrewvasquez0113@gmail.com>
2018-05-18 20:29:38 -0700

    got down basic javascript/jquery and now just have to stlye the page

---
+++ assets/javascript/game.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+    var random;
+    console.log(random);
+    var startReset = function() {
+        random = Math.floor(Math.random() * 101) + 19;
+        $("#rand-num").text(random);
+        for (i = 1; i < 5; i++) {
+            console.log("for loop ran");
+            var crysNum = Math.floor(Math.random() * 12) + 1;
+            console.log(crysNum);
+            $("#cry-" + i).attr("crysRandNum", crysNum);
+            console.log($("#cry-" + i).attr("crysRandNum"));
+        }
+        points = 0;
+        $("#points").text(points);
+    }
+    startReset();
+
+
+    var points = 0;
+
+    var winLose = function() {
+        if (points === random) {
+            alert("Win");
+            startReset();
+        }
+        else if (points > random) {
+            alert("Lose");
+            startReset();
+        }
+    }
+
+    $("#cry-1").on("click", function() {
+        console.log($("#cry-1").attr("crysRandNum"));
+        points = points + parseInt($("#cry-1").attr("crysRandNum"));
+        console.log(points);
+        $("#points").text(points);
+        winLose();
+    })
+
+    $("#cry-2").on("click", function() {
+        console.log($("#cry-2").attr("crysRandNum"));
+        points = points + parseInt($("#cry-2").attr("crysRandNum"));
+        console.log(points);
+        $("#points").text(points);
+        winLose();
+    })
+
+    $("#cry-3").on("click", function() {
+        console.log($("#cry-3").attr("crysRandNum"));
+        points = points + parseInt($("#cry-3").attr("crysRandNum"));
+        console.log(points);
+        $("#points").text(points);
+        winLose();
+    })
+
+    $("#cry-4").on("click", function() {
+        console.log($("#cry-4").attr("crysRandNum"));
+        points = points + parseInt($("#cry-4").attr("crysRandNum"));
+        console.log(points);
+        $("#points").text(points);
+        winLose();
+    })
+
+  });
